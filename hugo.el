4649e049e2342c738d00df5eefb99d00d5d2dea3
masa <masasam@users.noreply.github.com>
2017-03-13 20:34:09 +0900

    version 0.1

--- hugo.el
+++ hugo.el
@@ -4,7 +4,7 @@
 
 ;; Author: Masashi Miyaura
 ;; URL: https://github.com/masasam/emacs-hugo
-;; Version: 0.01
+;; Version: 0.1
 ;; Package-Requires: ((emacs "24.4"))
 
 ;; This program is free software; you can redistribute it and/or modify
